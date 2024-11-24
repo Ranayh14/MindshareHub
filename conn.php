e30254ab1fa0debb9e70a7062a28e146b7f1dfa0
Rana <ranayodahanifah@gmail.com>
2024-11-24 23:40:06 +0700

    koneksi

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";  
+$user = "root";       
+$password = "";       
+$database = "mindsharehub";  
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// Periksa koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
