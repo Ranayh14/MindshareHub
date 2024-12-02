25299c5325b6a567bdfd98a24fa25552399302f9
Rana <ranayodahanifah@gmail.com>
2024-12-02 23:45:11 +0700

    commit Rana1

--- conn.php
+++ conn.php
@@ -10,4 +10,6 @@ $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+date_default_timezone_set('Asia/Jakarta');
 ?>
